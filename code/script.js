04e348b67cff465c27c576c84210450f37dbc5ec
NCSources <104072199+NCResources@users.noreply.github.com>
2024-03-14 09:13:50 -0400

    Update script.js

--- code/script.js
+++ code/script.js
@@ -3,6 +3,6 @@ fetch('code.txt')
     .then(response => response.text())
     .then(codeData => {
         // Update the content in index.html
-        document.getElementById('content').innerText = codeData;
+        document.getElementById('code').innerText = codeData;
     })
     .catch(error => console.error('Error fetching code.txt:', error));
