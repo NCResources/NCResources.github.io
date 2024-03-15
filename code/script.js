b348aca9cfcbd99defdb77c030ab8e6a8dc20345
NCSources <104072199+NCResources@users.noreply.github.com>
2024-03-15 09:03:29 -0400

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,3 +1,4 @@
+function fetchCodes(){
 main-code = fetch("main-code.txt");
 mad-code = fetch("mad-code.txt");
 cookie-code = fetch ("cookie-code.txt")
@@ -5,3 +6,4 @@ cookie-code = fetch ("cookie-code.txt")
 document.getElementById('main-code').innerText = "Main: " + main-code;
 document.getElementById('mad-code').innerText = "Drive mad: " + mad-code;
 document.getElementById('cookie-code').innerText = "Cookie Clicker: " + cookie-code;
+}
