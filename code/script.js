7a02e0d0a82defc6f045ab29cfbe3da1504ebc14
NCSources <104072199+NCResources@users.noreply.github.com>
2024-05-22 02:06:14 -0400

    fix fetch

--- code/script.js
+++ code/script.js
@@ -1,9 +1,9 @@
-function fetchCodes(){
-const mainCode = fetch("main-code.txt");
-const madCode = fetch("mad-code.txt");
-const cookieCode = fetch ("cookie-code.txt")
+async function fetchCodes(){
+const mainCode = await fetch("main-code.txt");
+const madCode = await fetch("mad-code.txt");
+const cookieCode = await fetch ("cookie-code.txt")
 
-document.getElementById('main-code').innerText = "Main: " + mainCode;
-document.getElementById('mad-code').innerText = "Drive mad: " + madCode;
-document.getElementById('cookie-code').innerText = "Cookie Clicker: " + cookieCode;
+document.getElementById('main-code').innerText = `Main: ${mainCode}`;
+document.getElementById('mad-code').innerText = `Drive Mad: ${madCode}`;
+document.getElementById('cookie-code').innerText = `Cookie Clicker: ${cookieCode}`;
 }
